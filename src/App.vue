57a9ff5b7ba1b02d7d7fd5dca539d09724b591f1
OmarMontero <omamonalv@gmail.com>
2021-07-21 19:05:33 +0200

    Project finished

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <px-header />
+    <px-header :links="links" />
 
     <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
@@ -10,14 +10,33 @@
 import PxHeader from "@/components/PxHeader";
 
 export default {
-  name: "App",
+  name: "app",
   components: { PxHeader },
+
+  data() {
+    return {
+      links: [
+        {
+          title: "BTC",
+          to: { name: "coin-detail", params: { id: "bitcoin" } },
+        },
+        {
+          title: "ETH",
+          to: { name: "coin-detail", params: { id: "ethereum" } },
+        },
+        {
+          title: "XRP",
+          to: { name: "coin-detail", params: { id: "xrp" } },
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,12 +3,12 @@
     <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
       Proyecto para obtener las cotizaciones de las cryptomonedas mas
-      importantes a traves de la API REST de Coincap.
-      <br />Este proyecto es utilizado en el curo de Vue.js Basico de Platzi
+      importantes a través de la API REST de Coincap.
+      <br />Este proyecto es utilizado en el curso de Vue.js Basico de Platzi
     </p>
 
     <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
-      >Volver a la pagina de Inicio</router-link
+      >Volver a la página de Inicio</router-link
     >
   </div>
 </template>
